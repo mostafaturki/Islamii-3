96522593234aca0fc71adaa2df17dd81b5e6b449
Mostafa Torky <mostafaturki472018@gmail.com>
2025-03-13 04:23:11 +0200

    + Finished TasbeehFragment

--- app/src/main/java/com/route/islamii3/home/fragments/tasbeeh/TasbeehFragment.kt
+++ app/src/main/java/com/route/islamii3/home/fragments/tasbeeh/TasbeehFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.route.islamii3.R
 import com.route.islamii3.databinding.TasbeehFragmentBinding
 
 class TasbeehFragment: Fragment() {
+        var counter = 0
+        var currentIndex = 0
+        lateinit var azkarList: MutableList<String>
         lateinit var binding: TasbeehFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,5 +20,27 @@ class TasbeehFragment: Fragment() {
     ): View? {
         binding = TasbeehFragmentBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        azkarList = resources.getStringArray(R.array.azkarList).toMutableList()
+        binding.tvZekr.text = azkarList[0]
+        onSebhaClick()
+    }
+
+    private fun onSebhaClick() {
+        binding.ivSebhaBody.setOnClickListener{
+            binding.ivSebhaBody.rotation = (360/33).toFloat()
+
+            if (counter < 33){
+                counter++
+            }else {
+                counter = 0
+                currentIndex = if (currentIndex < azkarList.size-1) ++currentIndex else 0
+                binding.tvZekr.text = azkarList[currentIndex]
+            }
+            binding.tvTasbeehCounter.text = counter.toString()
+        }
     }
 }
